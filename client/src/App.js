14266948224dce84c0356dfd340ca8cdc17082a9
Akshit Thakur <akshit2649@gmail.com>
2022-10-23 16:26:11 +0530

    React js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
+
 function App() {
-  const Hello = "HEllO :)";
-  return <h1>{Hello}</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route>
+        {/*EndDash */}
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
