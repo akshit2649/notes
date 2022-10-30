19a7c5f29f3bcba1f2b4f19e6a6089b111d6d66a
Akshit Thakur <akshit2649@gmail.com>
2022-10-30 22:18:06 +0530

    typo in userName fixed

--- client/src/features/notes/NewNoteForm.jsx
+++ client/src/features/notes/NewNoteForm.jsx
@@ -39,8 +39,7 @@ const NewNoteForm = ({ users }) => {
   const options = users.map((user) => {
     return (
       <option key={user.id} value={user.id}>
-        {" "}
-        {user.username}
+        {user.userName}
       </option>
     );
   });
