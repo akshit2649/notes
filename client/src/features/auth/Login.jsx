d05e2cd9181f2fb30b2b4386310c6e777e99f5ca
Akshit Thakur <akshit2649@gmail.com>
2022-11-01 16:57:18 +0530

    login auth

--- client/src/features/auth/Login.jsx
+++ client/src/features/auth/Login.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import { useRef, useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { setCredentials } from "./authSlice";
+import { useLoginMutation } from "./authApiSlice";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const userRef = useRef();
+  const errRef = useRef();
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  useEffect(() => {
+    userRef.current.focus(); //puts the foucs
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [userName, password]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { accessToken } = await login({ userName, password }).unwrap(); //not using rtk states
+      dispatch(setCredentials({ accessToken }));
+      console.log("clicked");
+      setUserName("");
+      setPassword("");
+      navigate("/dash");
+    } catch (err) {
+      if (!err.status) {
+        setErrMsg("No Server Response");
+      } else if (err.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg(err.data?.message);
+      }
+      errRef.current.focus();
+    }
+  };
+
+  const handleUserInput = (e) => setUserName(e.target.value);
+  const handlePwdInput = (e) => setPassword(e.target.value);
+
+  const errClass = errMsg ? "errmsg" : "offscreen";
+
+  if (isLoading) return <p>Loading...</p>;
+
+  const content = (
+    <section className="public">
+      <header>
+        <h1>Employee Login</h1>
+      </header>
+      <main className="login">
+        <p ref={errRef} className={errClass}>
+          {errMsg}
+        </p>
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            className="form__input"
+            type="text"
+            id="username"
+            ref={userRef}
+            value={userName}
+            onChange={handleUserInput}
+            autoComplete="off"
+            required
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            className="form__input"
+            type="password"
+            id="password"
+            onChange={handlePwdInput}
+            value={password}
+            required
+          />
+          <button className="form__submit-button">Sign In</button>
+          {/*A single button inside of the form is by default a submit button*/}
+        </form>
+      </main>
+      <footer>
+        <Link to="/">Back to Home</Link>
+      </footer>
+    </section>
+  );
+
+  return content;
 };
 
 export default Login;
